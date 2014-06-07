08b5665d6c12423c7582349eb6f5763cd614adac
Jack Wearden <jack@jackwearden.co.uk>
2014-06-07 11:17:30 +0100

    support for HTML views

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ var express = require('express'),
     config  = require('./config'),
     routes  = require('./routes')(app);
 
+app.engine('html', mstx.express);
+app.set('view engine', mstx.express);
 app.set('views', __dirname + '/views');
+
+app.use('/static', express.static(__dirname + '/static'));
 
 app.listen(config.port);
 console.log("listening on port " + config.port);
